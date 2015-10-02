0d80753c199753c16484e48455ed597d51045acf
Clint Liddick <clint.liddick@gmail.com>
2015-10-02 15:14:43 -0400

    compiled, interim commit

--- src/tools_server.cpp
+++ src/tools_server.cpp
@@ -18,6 +18,7 @@
 #include <tabletop_perception_tools/PCLHelpers.h>
 #include <tabletop_perception_tools/ExtractClusters.h>
 #include <tabletop_perception_tools/FindBlocks.h>
+#include <tabletop_perception_tools/FindPrimitives.h>
 #include <visualization_msgs/MarkerArray.h>
 
 using namespace tabletop_perception_tools;
@@ -248,6 +249,187 @@ bool FindBlocksService(FindBlocks::Request& request, FindBlocks::Response& respo
         markers.markers.push_back(marker);
 
         response.blocks.push_back(block_msg);
+    }
+
+    vis_publisher.publish(markers);
+
+    ROS_INFO("Shutting down...");
+    sub.shutdown();
+
+    ROS_INFO("Done.");
+    return true;
+}
+
+bool FindPrimitivesService(FindPrimitives::Request& request, FindPrimitives::Response& response)
+{
+    std::string topic = request.point_cloud_topic;
+    pointCloudDirty = true;
+    ros::Subscriber sub = nodeHandle->subscribe(topic, 1, &PointCloudCallback);
+    ros::Duration timeout(5);
+    ros::Time begin = ros::Time::now();
+    ROS_INFO("Waiting for point cloud %s", topic.c_str());
+    while (pointCloudDirty)
+    {
+        usleep(10000);
+        ROS_INFO("Waiting...");
+        if (ros::Time::now() - begin > timeout)
+        {
+            ROS_WARN("Timeout exceeded waiting for point cloud %s", topic.c_str());
+            return false;
+        }
+    }
+
+    ROS_INFO("Got point cloud. Extracting");
+    PointCloud::Ptr pointCloud(new PointCloud(*lastPointCloud));
+
+    if (request.segment_depth)
+    {
+        ROS_INFO("Segmenting depth");
+        pointCloud = pcl_helpers::FilterBoundingBox<Point>(pointCloud, pcl_helpers::Vec3(-1000, -1000, request.min_depth), pcl_helpers::Vec3(1000, 1000, request.max_depth));
+        ROS_INFO("Filtered cloud has %lu points\n", pointCloud->size());
+    }
+
+    if (request.segment_box)
+    {
+        ROS_INFO("Segmenting box");
+        pointCloud = pcl_helpers::FilterBoundingBox<Point>(pointCloud, pcl_helpers::Vec3(request.box_min.x, request.box_min.y, request.box_min.z), pcl_helpers::Vec3(request.box_max.x, request.box_max.y, request.box_max.z));
+        ROS_INFO("Filtered cloud has %lu points\n", pointCloud->size());
+    }
+
+    if (request.segment_planes)
+    {
+        ROS_INFO("Removing planes");
+        pointCloud = pcl_helpers::RemoveLargePlanes<Point>(pointCloud, request.num_planes, request.plane_distance);
+        ROS_INFO("Filtered cloud has %lu points\n", pointCloud->size());
+    }
+
+    ROS_INFO("Filtered cloud has %lu points\n", pointCloud->size());
+    cloud_pub.publish(pointCloud);
+
+    ROS_INFO("Removing the clusters");
+    // std::vector<pcl_helpers::Cluster<Point> > clusters;
+    // pcl_helpers::ExtractClusters<Point>(pointCloud, &clusters, request.cluster_tolerance, request.min_cluster_size, request.max_cluster_size);
+    // Extract clusters
+    // Creating the KdTree object for the search method of the extraction
+    pcl::search::KdTree<Point>::Ptr tree (new pcl::search::KdTree<Point> ());
+    tree->setInputCloud(pointCloud);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<Point> ec;
+    ec.setClusterTolerance(0.2f);
+    ec.setMinClusterSize(100);
+    ec.setMaxClusterSize(25000);
+    ec.setSearchMethod(tree);
+    ec.setInputCloud(pointCloud);
+    ec.extract(cluster_indices);
+
+    // ROS_INFO("Extracted %lu clusters", clusters.size());
+    visualization_msgs::MarkerArray markers;
+    int j = 0;
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it)
+    // for (size_t i = 0; i < clusters.size(); i++)
+    {
+        pcl::PointCloud<Point>::Ptr cloud_cluster (new pcl::PointCloud<Point>);
+        for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit)
+        {
+          cloud_cluster->points.push_back(pointCloud->points[*pit]);
+        }
+        cloud_cluster->width = cloud_cluster->points.size();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true; // TODO necessary? correct?
+
+        // TODO do primitive segmentation on clusters
+
+        tabletop_perception_tools::Primitive prim_msg;
+        pcl::NormalEstimation<Point, pcl::Normal> ne;
+        pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+        // pcl::search::KdTree<Point>::Ptr tree (new pcl::search::KdTree<Point> ()); // TODO ok to reuse above?
+        pcl::SACSegmentationFromNormals<Point, pcl::Normal> seg;
+        pcl::ModelCoefficients::Ptr coefficients_cylinder (new pcl::ModelCoefficients);
+        pcl::PointIndices::Ptr inliers_cylinder (new pcl::PointIndices);
+
+        prim_msg.header.stamp = ros::Time::now(); // TODO get stamp from cloud
+        prim_msg.header.frame_id = pointCloud->header.frame_id;
+
+        // estimate point normals
+        ne.setSearchMethod(tree);
+        ne.setInputCloud(cloud_cluster);
+        ne.setKSearch(50); // TODO what is 50?
+        ne.compute(*cloud_normals);
+
+        seg.setOptimizeCoefficients(true);
+        seg.setModelType(pcl::SACMODEL_CYLINDER);
+        seg.setMethodType(pcl::SAC_RANSAC);
+        seg.setNormalDistanceWeight(0.1);
+        seg.setMaxIterations(10000);
+        seg.setRadiusLimits(0, 0.1); // TODO take as parameter
+        seg.setInputCloud(cloud_cluster);
+        seg.setInputNormals(cloud_normals);
+
+        seg.segment(*inliers_cylinder, *coefficients_cylinder);
+        ROS_DEBUG_STREAM("Cylinder coefficients: " << *coefficients_cylinder);
+
+        // TODO turn into ros pose
+
+        // TODO do other shapes and compare via some fit function
+
+
+        // block_msg.pose.position.x = cluster.bounds.transform.translation().x();
+        // block_msg.pose.position.y = cluster.bounds.transform.translation().y();
+        // block_msg.pose.position.z = cluster.bounds.transform.translation().z();
+
+        // pcl_helpers::Quaternion quat(cluster.bounds.transform.linear());
+
+        // block_msg.pose.orientation.x = quat.x();
+        // block_msg.pose.orientation.y = quat.y();
+        // block_msg.pose.orientation.z = quat.z();
+        // block_msg.pose.orientation.w = quat.w();
+
+        // float avg_r = 0.0f;
+        // float avg_g = 0.0f;
+        // float avg_b = 0.0f;
+        // float avg_a = 1.0f;
+        // for (size_t j = 0; j < cluster.points->size(); j++)
+        // {
+        //     const Point& p = cluster.points->at(j);
+        //     uint32_t rgb = *((int*)(&p.rgb));
+        //     uint8_t r = (rgb >> 16) & 0x0000ff;
+        //     uint8_t g = (rgb >> 8)  & 0x0000ff;
+        //     uint8_t b = (rgb)       & 0x0000ff;
+        //     avg_r += r;
+        //     avg_g += g;
+        //     avg_b += b;
+        // }
+
+        // avg_r /= cluster.points->size();
+        // avg_g /= cluster.points->size();
+        // avg_b /= cluster.points->size();
+
+        // avg_r /= 255.0f;
+        // avg_g /= 255.0f;
+        // avg_b /= 255.0f;
+
+        // block_msg.avg_color.r = avg_r;
+        // block_msg.avg_color.g = avg_g;
+        // block_msg.avg_color.b = avg_b;
+        // block_msg.avg_color.a = avg_a;
+
+        // visualization_msgs::Marker marker;
+        // marker.color.r = avg_r;
+        // marker.color.g = avg_g;
+        // marker.color.b = avg_b;
+        // marker.color.a = avg_a;
+        // marker.header.frame_id = block_msg.header.frame_id;
+        // marker.type = visualization_msgs::Marker::CUBE;
+        // marker.pose = block_msg.pose;
+        // marker.scale.x = 0.025f;
+        // marker.scale.y = 0.025f;
+        // marker.scale.z = 0.025f;
+        // marker.header.stamp = ros::Time::now();
+        // marker.id = i;
+        // markers.markers.push_back(marker);
+
+        // response.blocks.push_back(block_msg);
     }
 
     vis_publisher.publish(markers);
